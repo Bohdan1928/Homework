eb6bcdad218504c9f7b519d51bb892b9c75fb42d
Бодька <bbigun2000@gmail.com>
2020-10-29 12:36:11 +0200

    home

---
+++ JavaScript/home.js
@@ -0,0 +1,34 @@
+/* Task1
+let hello = "hello";
+let own = "own";
+let com = "com";
+let ua = "ua";
+let one = 1;
+let ten = 10;
+let minus = -999;
+let oneTowThree = 123;
+let pi = 3.14;
+let e = 2.7;
+let num = 16;
+let hasString = true;
+let hasNum = false;
+
+console.log(hasString, hasNum);
+alert(hello);
+alert(own);
+alert(com);
+alert(ua);
+alert(one);
+document.write("one, ten, minus, oneTowThree, pi, e, num) */
+
+
+let name = "Bogdan";
+let surname = "Bigun";
+let age = 18;
+
+alert("Name: " + name);
+alert("Surname: " + surname);
+alert("Age: " + age);
+document.write("Hi, my name is " + name + ", my surname is " + surname + ". I\'m " + age);
+console.log(name, surname, age)
+
