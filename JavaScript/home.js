2494ec9e3742514523453d0ea10bc063df1e1024
Бодька <bbigun2000@gmail.com>
2020-10-29 13:45:13 +0200

    home

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -22,13 +22,74 @@ alert(one);
 document.write("one, ten, minus, oneTowThree, pi, e, num) */
 
 
+/* Task2
 let name = "Bogdan";
-let surname = "Bigun";
+let lastname = "Bigun";
 let age = 18;
 
 alert("Name: " + name);
-alert("Surname: " + surname);
+alert("Surname: " + lastname);
 alert("Age: " + age);
 document.write("Hi, my name is " + name + ", my surname is " + surname + ". I\'m " + age);
-console.log(name, surname, age)
+console.log(name, surname, age)*/
+
+
+/* Task3
+const age = 52;
+const time = 16;
+const children = 5;
+const name = "Андрій";
+const lastname = "Іванович";
+const surname = "Садовий";
+
+console.log(name, lastname, surname, " вік: " + age, "на посаді: " + time, "дітей: " + children);
+document.write(name + " " + lastname + " " + surname, ", вік: " + age, ", на посаді: " + time, ", кількість дітей: " + children);
+alert(name);
+alert(lastname);
+alert(surname);
+alert("Вік: " + age);
+alert("На посаді: " + time);
+alert("Кількість дітей: " + children);
+*/
+
+
+/* Task4
+let lastname = prompt("What is your lastname?");
+let name = prompt("What is your name?");
+let surname = prompt("What is your surname?");
+console.log(lastname, name, surname);
+document.write(lastname + " " + name + " " + surname);
+alert(lastname);
+alert(name);
+alert(surname);*/
+
+
+/* Task5
+let lastname = prompt("What is your lastname?");
+let name = prompt("What is your name?");
+let surname = prompt("What is your surname?");
+document.write(lastname + " " + name + " " + surname);*/
+
+
+/* Task6
+let lastname = prompt("What is your lastname?")
+let name = prompt("What is your name?");
+let surname = prompt("What is your surname?");
+document.write(lastname + " " + name + " " + surname);
+
+let lastname2 = prompt("What is your lastname?");
+let name2 = prompt("What is your name?");
+let surname2 = prompt("What is your surname?");
+document.write(lastname2 + " " + name2 + " " + surname2);
+
+let lastname3 = prompt("What is your lastname?");
+let name3 = prompt("What is your name?");
+let surname3 = prompt("What is your surname?");
+document.write(lastname3 + " " + name3 + " " + surname3);
+
+let lastname4 = prompt("What is your lastname?");
+let name4 = prompt("What is your name?");
+let surname4 = prompt("What is your surname?");
+document.write(lastname4 + " " + name4 + " " + surname4);*/
+
 
