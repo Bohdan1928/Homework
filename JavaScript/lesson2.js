8f9149afda288fe13cc407f47869b47b835cec7a
Бодька <bbigun2000@gmail.com>
2020-10-30 18:13:57 +0200

    home

---
+++ JavaScript/lesson2.js
@@ -0,0 +1,144 @@
+/* Task1
+let numbers = [0, 1, 2, 3, 4];
+console.log(numbers);
+let string = ["null", "one", "two", "three", "four"];
+console.log(string);
+let all = [60, true, "false", "12", false];
+console.log(all);*/
+
+
+
+/* Task2
+for (let i = 0; i < 10; i++){
+    document.write(`<div>text</div>`);
+}*/
+
+
+/* task3
+for (let i = 0;i < 10;i++){
+    document.write(`<div>text${i}</div>`);
+}*/
+
+/* Task4-5
+let start = 19;
+while (start >= 0){
+    document.write(`<h1>text${start}</h1>`);
+    start--
+}*/
+
+
+/* Task6
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+for(let i = 0; i < numbers.length; i++){
+    console.log(numbers[i]);
+}*/
+
+
+/* Task7
+let string = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+for(let i = 0; i < string.length; i++) {
+    console.log(string[i]);
+}*/
+
+
+/* Task8
+let all = [0, "two", 2, false, "true", true, "six", 7, 8, "nine"];
+for(let i = 0; i < all.length; i++) {
+    console.log(all[i]);
+}*/
+
+
+/* Task9
+let all = [0, "two", 2, false, "true", true, "six", 7, 8, "nine"];
+for(let i = 0; i < all.length; i++) {
+    if (typeof all[i] === `boolean`){
+        console.log(all[i])
+    }
+}*/
+
+/* Task10
+let all = [0, "two", 2, false, "true", true, "six", 7, 8, "nine"];
+for(let i = 0; i < all.length; i++) {
+    if (typeof all[i] === `number`) {
+        console.log(all[i])
+    }
+}*/
+
+
+/* Task11
+let all = [0, "two", 2, false, "true", true, "six", 7, 8, "nine"];
+for(let i = 0; i < all.length; i++) {
+    if (typeof all[i] === `string`) {
+        console.log(all[i])
+    }
+}*/
+
+
+/* Task12
+let all = [];
+all[0] = "hello";
+all[1] = 2;
+all[2] = 9;
+all[3] = true;
+all[4] = "three";
+all[5] = "six";
+all[6] = false;
+all[7] = 6;
+all[8] = "five";
+all[9] = true;
+for(let i = 0; i < all.length; i++){
+    console.log(all[i]);
+}*/
+
+
+/* Task13
+for(let i = 0; i < 10; i++){
+    console.log(i);
+    document.write(i);
+}*/
+
+
+/* Task14
+for(let i = 0; i < 100; i++){
+    console.log(i);
+    document.write(i);
+}*/
+
+
+/* Task15
+for(let i = 0; i < 100; i+=2){
+    console.log(i/2);
+    document.write(i/2);
+}*/
+
+
+/* Task16
+for(let i = 2; i < 100; i+=2) {
+    console.log(i);
+    document.write(i);
+}*/
+
+
+/* Task17
+for(let i = 1; i < 100; i+=2) {
+    console.log(i);
+    document.write(i);
+}*/
+
+
+/* Task18
+for(let i = 0; i < 2; i++) {
+    for (let k =0; k < 60; k++){
+        console.log(i, k);
+    }
+}*/
+
+
+/* Task19
+for(let i = 0; i < 2; i++) {
+    for (let k =0; k < 20; k++){
+        for(let j = 0; j < 60; j++) {
+            console.log(i, k, j);
+        }
+    }
+}*/
