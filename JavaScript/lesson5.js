7d2a951958fd67992f2cfd406b6fab642fbb2b38
Бодька <bbigun2000@gmail.com>
2020-11-10 16:52:24 +0200

    home

--- JavaScript/lesson5.js
+++ JavaScript/lesson5.js
@@ -109,85 +109,181 @@ car.addDriver({
 
 console.log(car);*/
 
+/*
+ Task 4
+function car(model, maker, years, maxSpeed, value) {
+    this.model = model;
+    this.maker = maker;
+    this.year = years;
+    this.maxSpeed = maxSpeed;
+    this.value = value;
 
-/* Task 4
-function add(one) {
-    let car = {
-        model: "Toyota Corolla",
-        maker: "Toyota-motors",
-        years: 2015,
-        maxSpeed: 250,
-        value: 2.5,
-        drive() {
-            `Їдемо зі швидкістю ${this.maxSpeed} км/год`
-        },
-        info() {
-            console.log(car.model, car.maker, car.years, car.maxSpeed, car.value);
-        },
-        increaseMaxSpeed(newSpeed) {
-            car.maxSpeed += newSpeed;
-            console.log(car.maxSpeed);
-        },
-        changeYear(newValue) {
-            car.value += newValue;
-            console.log(car.value);
-        },
-        addDriver(driver) {
-            this.driver1 = driver
-        },
+    this.drive = function () {
+        console.log(`Їдемо зі швидкістю ${this.maxSpeed} км/год `);
     }
-    car.drive();
-    car.info();
-    car.increaseMaxSpeed(25);
-    car.changeYear(0.5);
-    car.addDriver({
-        name: "Ihor",
-        age: 23,
-        exp: 3
-    })
-    console.log(car);
+    this.info = function () {
+        console.log(this.model, this.maker, this.years, this.maxSpeed, this.value);
+    }
+    this.increaseMaxSpeed = function (newSpeed) {
+        this.maxSpeed += newSpeed;
+        console.log(this.maxSpeed);
+    }
+    this.changeYear = function (newValue) {
+        this.year += newValue;
+        console.log(this.year);
+    }
+    this.addDriver = function (driver) {
+        this.driver1 = driver
+    }
+    this.ride = function (){
+        console.log(this)
+    }
 }
+let toyota = new car("Toyota Corolla", "Toyota-motors", 2015, 250, 2.0)
+let mazda = new car("mazda 6", "Mazda-motors",2016, 275, 2.4)
 
-add();
+toyota.drive();
+toyota.info();
+toyota.increaseMaxSpeed(20);
+toyota.changeYear(4);
+toyota.addDriver({
+    name: "Ihor",
+    age: 23,
+    exp: 3,
+})
+toyota.ride()
+
+mazda.drive();
+mazda.info();
+mazda.increaseMaxSpeed(20);
+mazda.changeYear(4);
+mazda.addDriver({
+    name: "Ihor",
+    age: 23,
+    exp: 3,
+})
+mazda.ride()
 */
 
-
-/* Task 5
+/*/!* Task 5*!/
 class Add {
-    constructor() {
-        let car = {
-            model: "Toyota Corolla",
-            maker: "Toyota-motors",
-            years: 2015,
-            maxSpeed: 250,
-            value: 2.5,
-            drive() {
-                `Їдемо зі швидкістю ${this.maxSpeed} км/год`
-            },
-            info() {
-                console.log(car.model, car.maker, car.years, car.maxSpeed, car.value);
-            },
-            increaseMaxSpeed(newSpeed) {
-                car.maxSpeed += newSpeed;
-                console.log(car.maxSpeed);
-            },
-            changeYear(newValue) {
-                car.value += newValue;
-                console.log(car.value);
-            },
-            addDriver(driver) {
-                this.driver1 = driver
-            },
-        }
-        car.drive();
-        car.info();
-        car.increaseMaxSpeed(25);
-        car.changeYear(0.5);
-        car.addDriver({
+    constructor(model, maker, years, maxSpeed, value, ) {
+        this.model = model;
+        this.maker = maker;
+        this.year = years;
+        this.maxSpeed = maxSpeed;
+        this.value = value;
+    }
+    drive() {
+        console.log(`Їдемо зі швидкістю ${this.maxSpeed} км/год`);
+    }
+    info() {
+        console.log(this.model, this.maker, this.year, this.maxSpeed, this.value);
+    }
+    increaseMaxSpeed(newSpeed) {
+        this.maxSpeed += newSpeed;
+        console.log(this.maxSpeed);
+    }
+    changeYear(newValue) {
+        this.year += newValue;
+        console.log(this.year);
+    }
+    addDriver(driver) {
+        this.driver1 = driver
+    }
+    ride = function (){
+        console.log(this)
+    }
+}
+
+let toyota = new Add("Toyota Corolla", "Toyota-motors", 2015, 250, 2.0)
+let mazda = new Add("mazda 6", "Mazda-motors",2016, 275, 2.4)
+
+toyota.drive();
+toyota.info();
+toyota.increaseMaxSpeed(25);
+toyota.changeYear(4);
+toyota.addDriver({
             name: "Ihor",
             age: 23,
             exp: 3
         })
-        console.log(car);
+toyota.ride()
+
+mazda.drive();
+mazda.info();
+mazda.increaseMaxSpeed(25);
+mazda.changeYear(4);
+mazda.addDriver({
+    name: "Ihor",
+    age: 23,
+    exp: 3
+})
+mazda.ride()*/
+
+/* Task 6
+class Pop {
+    constructor(name, age, foot) {
+        this.name = name;
+        this.age = age;
+        this.foot = foot;
+    }
+}
+class Pri {
+    constructor(name, age, find){
+        this.name = name;
+        this.age = age;
+        this.find = find;
+    }
+}
+let pop =[
+    new Pop("Diana","16", 37),
+    new Pop("Ira","18", 38),
+    new Pop("Nadia","20", 39.5),
+    new Pop("Natalia","21", 39),
+    new Pop("Lily","18", 40),
+    new Pop("Rosa","17", 35),
+    new Pop("Karina","19", 41),
+    new Pop("Yulia","18", 36.5),
+    new Pop("Marta","17", 37.5),
+    new Pop("Olga","22", 40.5)
+];
+
+let pri = new Pri("Bohdan", 18, 37)
+
+for (let i = 0; i < pop.length; i++) {
+    if(pop[i].foot === pri.find){
+        console.log(pop[i]);
+    }
+}*/
+
+/* Task 7
+function popFunc(name, age, foot) {
+    this.name = name;
+    this.age = age;
+    this.foot = foot;
+}
+function priFunc(name, age, find) {
+    this.name = name;
+    this.age = age;
+    this.find = find;
+}
+let pop =[
+    new popFunc("Diana","16", 37),
+    new popFunc("Ira","18", 38),
+    new popFunc("Nadia","20", 39.5),
+    new popFunc("Natalia","21", 39),
+    new popFunc("Lily","18", 40),
+    new popFunc("Rosa","17", 35),
+    new popFunc("Karina","19", 41),
+    new popFunc("Yulia","18", 36.5),
+    new popFunc("Marta","17", 37.5),
+    new popFunc("Olga","22", 40.5)
+];
+let pri = new priFunc("Bohdan", 18, 37)
+
+for (let i = 0; i < pop.length; i++) {
+    if (pop[i].foot === pri.find) {
+        console.log(pop[i]);
     }
 }*/
