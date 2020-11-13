29987d254c8841955a45441236e783df1f9116c5
Бодька <bbigun2000@gmail.com>
2020-11-13 19:41:50 +0200

    home

---
+++ JavaScript/lesson8.js
@@ -0,0 +1,28 @@
+/* Task1
+let wrap4 = document.getElementById("wrap4")
+
+wrap4.onclick = () =>{
+    if (wrap4.innerText){
+        wrap4.innerText = ""
+    }
+    return
+}*/
+
+
+/* Task2
+let submit = document.getElementById("wrap1");
+submit.onclick = () => {
+    submit.innerText = "";
+    submit.style.backgroundColor = "white";
+}
+*/
+
+/*
+let red = document.getElementById("wrap2")
+
+red.onclick = () =>{
+    document.forms.myForm.someText.value = "123"
+}
+
+*/
+
