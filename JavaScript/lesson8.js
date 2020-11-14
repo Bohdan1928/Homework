b8b93f5ae1b623133423f4a16d357e316b2f86d2
Бодька <bbigun2000@gmail.com>
2020-11-14 03:01:22 +0200

    home

--- JavaScript/lesson8.js
+++ JavaScript/lesson8.js
@@ -9,20 +9,37 @@ wrap4.onclick = () =>{
 }*/
 
 
-/* Task2
+/* Task3
 let submit = document.getElementById("wrap1");
 submit.onclick = () => {
     submit.innerText = "";
     submit.style.backgroundColor = "white";
-}
-*/
+}*/
 
-/*
+
+/* Task5
+let submit = document.forms.myForm2.submit
+document.forms.myForm2.submit.addEventListener(`click`,(ev) => {ev.preventDefault()})
+
+submit.onclick = () => {
+    if (myForm2.someText.value >= 18) {
+        alert("Вітаю, тобі більше 18")
+    } else{
+       alert("Сорі, але тобі менше 18")
+    }
+}*/
+
+
+/* Task7!
 let red = document.getElementById("wrap2")
+let black = document.getElementById("wrap2-1")
+black.style.display = "none"
 
-red.onclick = () =>{
-    document.forms.myForm.someText.value = "123"
-}
+red.addEventListener("click",(ev) =>{
+    black.style.display = "block"
+})
 
-*/
-
+black.addEventListener("click",(ev) =>{
+    red.style.display = "block"
+    black.style.display = "none"
+})*/
