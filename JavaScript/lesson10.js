7b9a7ea1a2b1a14c781d544f3df4ce0d38eaf067
Бодька <bbigun2000@gmail.com>
2020-11-21 00:34:48 +0200

    home

--- JavaScript/lesson10.js
+++ JavaScript/lesson10.js
@@ -24,10 +24,60 @@ function univer(aud, callback){
 }
 
 function lifeInUniver(money, callback){
-    callback("Пара акінчиласью Йду в кормушку")
+    callback("Пара закінчилась, Йду в кормушку")
 
     if (money < 20){
-        callback("Вигнали з кормушки. Помер з голоду")
+        callback("Вигнали з кормушки.", null)
+    } else {
+        callback(null ,"Купив котлетку з бульбою,")
+    }
+}
+
+function lifeInUniver2(time, callback){
+    callback("Прийшов на ту довбану пару")
+
+    if(time < 15){
+        callback("Правило \" 15 хв\" ніхто не відміняв", null)
+        callback("Валимо...", null)
+    } else{
+        callback(null, "Правило \"15 хв\" не спрацювало, вона прийшла")
+    }
+}
+
+function shavuha(callback){
+    callback("Пара закінчилась, вийшов з того універу")
+    callback("Без шавухи нікуди")
+}
+
+function repa(metronom, callback){
+    callback("Пара закінчилась, вийшов з того універу")
+    callback("Без шавухи нікуди")
+    callback("Поїв, можна йти на рєпу")
+
+    if(metronom > 120){
+        callback(null ,"Метроном є, тому граю файно)))")
+    } else{
+        callback("Забув метроном, пічально", null)
+    }
+}
+
+function goHome(num, callback){
+    callback("Рєпа закінчилась можна їхати додому")
+
+    if(num === 46){
+        callback(null ,"О, є маршрутка, їду додому")
+    } else{
+        callback("Шкода що нема маршрутки, прийдеться йти пішки", null)
+    }
+}
+function inHome(dz, callback){
+        callback("Приїхав додому")
+
+
+    if (dz === 0){
+        callback(null ,"На дз нічого, тому: \"Добраніч, солодких снів\"")
+    } else {
+        callback("Міша, всьо фігня, давай по-новій", null)
     }
 }
 life(9,(err, un) =>{
@@ -36,19 +86,69 @@ life(9,(err, un) =>{
     } else{
         console.log("Сів в маршрутку.", un);
         setTimeout(() => {
-
             console.log("Я на місці");
+
+            univer(5, (err, good) => {
+                setTimeout(()=>{
+                    if(err){
+                        console.log(err);
+                    } else {
+                        console.log(good);
+
+                        lifeInUniver(30, (err, good) => {
+                            setTimeout(() => {
+                                if(err){
+                                    console.log(err);
+                                } else{
+                                    console.log(good, "йду довольний на наступну пару");
+
+                                    lifeInUniver2(20, (err, good)=> {
+                                        setTimeout(() =>{
+                                            if(err){
+                                                console.log(err);
+                                            } else{
+                                                console.log(good);
+
+                                                repa(121, (err, good) => {
+                                                    setTimeout(()=>{
+                                                        if(err){
+                                                            console.log(err);
+                                                        } else{
+                                                            console.log(good);
+
+                                                            goHome(46, (err, good) => {
+                                                                setTimeout(() =>{
+
+                                                                    if(err){
+                                                                        console.log(err);
+                                                                    } else{
+                                                                        console.log(good);
+
+                                                                        inHome(0, (err, good) => {
+                                                                            setTimeout(() =>{
+                                                                                if(err){
+                                                                                    console.log(err);
+                                                                                } else{
+                                                                                    console.log(good);
+                                                                                }
+                                                                            }, 2000)
+                                                                        })
+                                                                    }
+                                                                }, 2000)
+                                                            })
+                                                        }
+                                                    }, 2000)
+                                                })
+
+                                            }
+                                        }, 2000)
+                                    })
+                                }
+                            }, 2000)
+                        })
+                    }
+                },1000)
+            })
         }, 1000)
-        univer(5, (err, good) => {
-            setTimeout(()=>{
-                if(err){
-                    console.log(err);
-                } else {
-                    console.log(good);
-                }
-            }, 2000)
-
-        })
-
     }
 })
